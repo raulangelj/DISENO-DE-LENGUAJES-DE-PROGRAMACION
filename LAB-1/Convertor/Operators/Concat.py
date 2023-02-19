43d85c1b56ad7a03610012967047cdd6e9fadbd6
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-02-18 18:44:43 -0600

    FIX rules concat and plus

--- LAB-1/Convertor/Operators/Concat.py
+++ LAB-1/Convertor/Operators/Concat.py
@@ -7,6 +7,8 @@ class Concat(Operator):
 		super().__init__()
 		self.simbol = CONCATENATION_SIMBOL
 		self.priority = 2
+		self.simbols = [self.kleene_simbol, self.simbol,
+						self.union_simbol, self.optional_simbol, self.plus_simbol]
 
 	'''
 		Validate the expression
@@ -14,15 +16,50 @@ class Concat(Operator):
 		@param index: index of the factor to validate
 		@return: expression validated (str)
 	'''
-	def validate(self, factors, index = 0):
-		final_exp =''
+
+	def validate(self, factors, index=0):
+		final_exp = ''
 		for index in range(len(factors)):
+			is_open_agrupation = factors[index] == self.agrupation[0]
+			is_not_two_agrupation_together = (
+				factors[index] not in self.agrupation and factors[index + 1 if index < len(factors) - 1 else index] is self.agrupation[1])
+			is_last_factor = index == len(factors) - 1
+			has_more_than_one_factor = len(factors) > 1
 			# ['(', ...], ['a', ')'], ['a']
-			if factors[index] == self.agrupation[0] or (factors[index] not in self.agrupation and factors[index + 1 if index < len(factors) - 1 else index] is self.agrupation[1]) or (index == len(factors) - 1):
+			if (is_open_agrupation or is_not_two_agrupation_together or is_last_factor) and has_more_than_one_factor:
+				final_exp += factors[index]
+			# [')', ...]
+			elif factors[index] == self.agrupation[1] and factors[index + 1 if index < len(factors) - 1 else index] in self.simbols:
 				final_exp += factors[index]
 			# ['a|', ...]
-			elif len(factors[index]) > 1 and factors[index][-1] ==  UNION_SIMBOL:
+			elif len(factors[index]) > 1 and factors[index][-1] == UNION_SIMBOL:
+				final_exp += factors[index]
+			# ['(aa)', ...]
+			elif len(factors[index]) > 1 and factors[index][0] == self.agrupation[0] and factors[index][-1] == self.agrupation[1]:
+				final_exp += f'{self.rules_concat(factors[index])}{self.simbol if index < len(factors) - 1 else ""}'
+			# ['a']
+			elif index == len(factors) - 1:
 				final_exp += factors[index]
 			else:
 				final_exp += f'{factors[index]}{self.simbol}'
 		return final_exp, 0
+
+	def rules_concat(self, agrupation):
+		expresion_concat = ''
+		for index in range(len(agrupation)):
+			caracter = agrupation[index]
+			next_index = index + 1 if index < len(agrupation) - 1 else index
+			if self.is_close_open_agrupation(caracter, agrupation[next_index]) or self.is_two_letters(caracter, agrupation[next_index]):
+				expresion_concat += f'{caracter}{self.simbol}'
+			else:
+				expresion_concat += caracter
+		return expresion_concat
+
+	def is_close_open_agrupation(self, caracter, next_caracter):
+		return caracter is self.agrupation[1] and next_caracter is self.agrupation[0]
+
+	def is_simbol(self, caracter):
+		return caracter in self.simbols + self.agrupation
+	
+	def is_two_letters(self, caracter, next_caracter):
+		return not self.is_simbol(caracter) and not self.is_simbol(next_caracter)
--- LAB-1/Convertor/SpecialOperators/Plus.py
+++ LAB-1/Convertor/SpecialOperators/Plus.py
@@ -14,13 +14,14 @@ class Plus(Operator):
 		caracter_before = factors[-1] if len(factors) > 0 else ''
 		if len(factors) == 0 or caracter_before is self.union_simbol:
 			raise Exception(self.errors[0])
-		# [')', actual ...]
 		elif caracter_before is self.agrupation[1]:
 			agrupation_plus, position_move = self.find_agrupation(factors)
 			return f'{self.agrupation[0]}{agrupation_plus}{self.concatenation_simbol}{agrupation_plus}{self.kleene_simbol}{self.agrupation[1]}', position_move
-		# ['a|b', actual ...]
 		elif len(caracter_before) > 1 and caracter_before[-1] is not self.agrupation[1]:
 			letter = caracter_before[-1]
 			return f'{caracter_before[:-1]}{self.agrupation[0]}{letter}{self.concatenation_simbol}{letter}{self.kleene_simbol}{self.agrupation[1]}', 1
+		elif len(caracter_before) > 1:
+			agrupation_plus, position_move = self.find_agrupation("".join(factors))
+			return f'{caracter_before[:-position_move]}{self.agrupation[0]}{agrupation_plus}{self.concatenation_simbol}{agrupation_plus}{self.kleene_simbol}{self.agrupation[1]}', position_move
 		else:
 			return f'{self.agrupation[0]}{caracter_before}{self.concatenation_simbol}{caracter_before}{self.kleene_simbol}{self.agrupation[1]}', 1

2f0a6cd9c7661b5f155c64da296c5b6ef3b4acbe
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-02-20 19:28:10 -0600

    ADD: token class

---
+++ LAB-1/Automata/Token.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from typing import TypedDict, List
+
+class TokenModel():
+	value: str
+	ascii: int
+
+
+class Token():
+	def __init__(self, value):
+		self.value = value
+		self.ascii = ord(value)
+	
+	def model(self) -> TokenModel:
+		return {
+			'value': self.value,
+			'ascii': self.ascii
+		}
+
+	def __str__(self) -> str:
+		return f'value: {self.value}, ascii: {self.ascii}'
+
+	def is_in(self, tokens: List[Token]) -> bool:
+		return any(token.value == self.value for token in tokens)
+
+	def print_token(self) -> None:
+		print("+--------------------+----------+")
+		print("| Token              | Value    |")
+		print("+--------------------+----------+")
+		print("|{:<20}|{:>10}|".format('Value', self.value))
+		print("|{:<20}|{:>10}|".format('Ascii', self.ascii))
+		print("+--------------------+----------+")

5167d578b5d25e5713c38b6761e637aa118b5cc7
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-02-22 13:14:19 -0600

    ADD: menu class

--- LAB-1/Menu/Menu.py
+++ LAB-1/Menu/Menu.py
@@ -1,4 +1,19 @@
-initial_menu = '''
-  1. Input a Regex
-  2. Exit
-'''
+from pick import pick
+from typing import Tuple
+
+class Menu():
+	def __init__(self) -> Tuple[str, int]:
+		self.option: int = 0
+		self.option_value: str = ''
+
+	def print_main_menu(self):
+		title = 'Please choose action to do: '
+		options = ['Enter a new regex expression', 'Run tests', 'Exit']
+		return pick(
+			options,
+			title,
+			multiselect=False,
+			default_index=0,
+			min_selection_count=1,
+			indicator='->',
+		)

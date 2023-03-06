31082e5da56afcd219be2ac41f01b3dc1f8a21b2
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-03-05 20:29:53 -0600

    ADD NFA simulation and start Subsets

--- LEXER/Automata/NFA.py
+++ LEXER/Automata/NFA.py
@@ -1,9 +1,12 @@
 from typing import List
 import graphviz as gv
-from graphviz import Source
+from termcolor import colored
 from Automata.Automata import Automata
-from Convertor.Simbols import Operators
 from Automata.State import State
+from Automata.Token import Token
+from Automata.EmptyToken import EmptyToken
+from Convertor.Simbols import Operators
+from Automata.DFA import DFA
 
 class NFA(Automata):
 	def __init__(self) -> None:
@@ -54,14 +57,55 @@ class NFA(Automata):
 		dot.attr(rankdir='LR', label=f'NFA Graph: {self.original}')
 		dot.engine = 'dot'
 		# Nodes to graph
+		dot.node('initial', 'initial', shape='point')
 		for state in self.states:
 			if state.is_final:
 				dot.node(str(state.value), str(state.value), shape='doublecircle')
-			elif state.is_initial:
-				dot.node(str(state.value), str(state.value), shape='point')
 			else:
 				dot.node(str(state.value), str(state.value), shape='circle')
 		# Edges to graph
+		dot.edge('initial', str(self.initial_state.value))
 		for state_1, token, state_2 in self.transitions.transitions:
 			dot.edge(str(state_1.value), str(state_2.value), label=str(token.value))
 		dot.render(f'LEXER/NFA_GRAPH/{fileName}.gv', view=True)
+
+	def e_closure(self, states: List[State]):
+		e_closure = states.copy()
+		for state in e_closure:
+			for initial_state, token, final_state in self.transitions.transitions:
+				if (
+					initial_state == state
+					and token.value == EmptyToken().value
+					and final_state not in e_closure
+				):
+					e_closure.append(final_state)
+		return e_closure
+
+
+	def move(self, states: List[State], token_to_evaluate: Token) -> List[State]:
+		move = [] + states
+		for state in states:
+			for initia_state, token, final_state in self.transitions.transitions:
+				if (
+					initia_state == state
+					and token.value == token_to_evaluate.value
+					and final_state not in move
+				):
+					move.append(final_state)
+		return move
+	
+	def simulate(self, input_string: str) -> str:
+		current_states = self.e_closure([self.initial_state])
+		for caracter in input_string:
+			current_states = self.e_closure(self.move(current_states, Token(caracter)))
+		is_valid = any(state.is_final for state in current_states)
+		if is_valid:
+			return colored(f'"{input_string}" is valid', 'green')
+		else:
+			return colored(f'"{input_string}" is invalid', 'red')
+	
+	def DFA_subsets(self):
+		dfa = DFA()
+		temporal_states = []
+		dfa_states = self.e_closure([self.initial_state])
+		print('=>dfa_states: ', dfa_states)
--- LEXER/Controller.py
+++ LEXER/Controller.py
@@ -1,10 +1,12 @@
 from Convertor.Parser import Parser
 from Automata.NFA import NFA
+from Automata.DFA import DFA
 
 class Controller():
 	def __init__(self) -> None:
 		self.parser: Parser = Parser()
 		self.nfa: NFA = NFA()
+		self.dfa = DFA()
 
 	def convert_infix_to_postfix(self, infix: str) -> str:
 		self.original_infix = infix
@@ -16,5 +18,11 @@ class Controller():
 
 	def render_graph(self, file_name:str='') -> None:
 		self.nfa.create_graph(file_name)
+
+	def get_dfa_subsets(self) -> None:
+		self.dfa = self.nfa.DFA_subsets()
+
+	def simulate_nfa(self, input_string: str) -> str:
+		return self.nfa.simulate(input_string)
 
 	

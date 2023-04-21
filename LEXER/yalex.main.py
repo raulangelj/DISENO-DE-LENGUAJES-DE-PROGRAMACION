04e0df6d7b72e15fa681d266b6af0b1f6e475fe5
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-04-21 16:08:56 -0600

    ADD create scanner

--- LEXER/yalex.main.py
+++ LEXER/yalex.main.py
@@ -1,3 +1,6 @@
+import os
+import glob
+import pickle
 from YALEX.YALEX import Yalex
 from termcolor import colored
 from Convertor.ConvertorAlgorithms import Algorithms
@@ -6,6 +9,63 @@ from Tree.Tree import Tree
 from Automata.DFA import DFA
 from Convertor.Character import *
 
+RUTE_COMPILER = 'LEXER/'
+
+compiler_content = '''from Automata.DFA import DFA
+from Convertor.Character import *
+import os
+import pickle
+
+def main():
+    file_path = input('Ingrese la ruta del archivo: ')
+    file_data = ''
+    with open(file_path, 'r') as file:
+        for line in file:
+            for character in line:
+                file_data += character
+    tokens = None
+    with open('LEXER/automata.pkl', 'rb') as file:
+        automata = pickle.load(file)
+        sentence = Characters(file_data)
+        tokens = automata.simulate(sentence)
+    for token in tokens:
+        if token.token_type.value != 1:
+            return_val = token.accepted_state.return_value.value[1:-1].strip() if token.accepted_state.return_value else ''
+            exec(return_val)
+        else:
+            print(f'TOKEN NO VALIDO: {token.characters}')
+if __name__ == '__main__':
+    main()
+'''
+
+def create_scanner():
+    # # create the deps files
+    # directories = ['LEXER/Automata', 'LEXER/Convertor', 'LEXER/Tree', 'LEXER/YALEX']
+    # # directories = ['LEXER/Tree', 'LEXER/YALEX']
+    # py_files = []
+    # for directory in directories:
+    #     py_files += glob.glob(os.path.join(directory, '*.py'))
+    # # create the vendor.py
+    # new_file = 'vendor.py'
+    # with open(f'{RUTE_COMPILER}{new_file}', 'w') as f:
+    #     # iterate over each .py file
+    #     for py_file in py_files:
+    #         with open(py_file, 'r') as g:
+    #             # read the contents of the .py file
+    #             contents = g.readlines()
+    #             # write the contents to the new file, skipping any import lines
+    #             for line in contents:
+    #                 if 'import' not in line or 'typing' in line:
+    #                     f.write(line)
+    #         f.write('\n')
+    # create another file name as scanner.py in which when run it will ask the user for a file path to open that file and print all the characters in the file
+    # create the directory if it does not exist
+    if not os.path.exists(RUTE_COMPILER):
+        os.makedirs(RUTE_COMPILER)
+    with open(f'{RUTE_COMPILER}scanner.py', 'w') as file:
+        file.write(compiler_content)
+    # run the file created
+    # os.system(f'python {RUTE_COMPILER}scanner.py')
 
 def main():
     running = True
@@ -16,7 +76,8 @@ def main():
         option = input('Ingrese una opción: ')
         if option == '1':
             try:
-                file_path = input('Ingrese la ruta del archivo: ')
+                # file_path = input('Ingrese la ruta del archivo: ')
+                file_path = 'LEXER/Mocks/YALEX/slr-4.yal'
                 algorithms = Algorithms()
                 yalex = Yalex(file_path)
                 yalex.read_file()
@@ -41,14 +102,19 @@ def main():
                 # print(adf.simulate(')'))
                 # print(adf.simulate('11.11E-11'))
                 # print(adf.simulate('1.1.1.E15'))
-                text_to_analyze = ''
-                # open file cualquiera.txt that is in the same folder as this file
-                with open('LEXER/cualquiera.txt', 'r') as file:
-                    for line in file:
-                        for character in line:
-                            text_to_analyze += character
-                sentece = Characters(text_to_analyze)
-                print(adf.simulate(sentece))
+                # create a pickle file with the automata
+                with open(f'{RUTE_COMPILER}automata.pkl', 'wb') as file:
+                    pickle.dump(adf, file)
+
+                create_scanner()
+                # text_to_analyze = ''
+                # # open file cualquiera.txt that is in the same folder as this file
+                # with open('LEXER/cualquiera.txt', 'r') as file:
+                #     for line in file:
+                #         for character in line:
+                #             text_to_analyze += character
+                # sentece = Characters(text_to_analyze)
+                # print(adf.simulate(sentece))
                             # tree.followpos_recursive(tree.tree)
                             # dfa = DFA()
                             # dfa.create_automata(postfix=postfix, tree=tree, infix=yalex.expression, originial=yalex.expression)

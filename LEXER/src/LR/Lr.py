1ed6d870f740e3f339fe5720f0c520615a124969
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-05-12 18:51:14 -0600

    REF remove un used comments and improve code

--- LEXER/src/LR/Lr.py
+++ LEXER/src/LR/Lr.py
@@ -81,21 +81,17 @@ class Lr0():
     def is_same_list_productions(self, first: List[Production], second: List[Production]) -> bool:
         if len(first) != len(second):
             return False
-        for item in first:
-            if item not in second:
-                return False
-        return True
+        return all(item in second for item in first)
     
     def is_in_list_production(self, production: Production, productions: List[List[Production]]) -> bool:
-        for item in productions:
-            if self.is_same_list_productions(item, production):
-                return True
-        return False
+        return any(
+            self.is_same_list_productions(item, production) for item in productions
+        )
     
     def print_list_production(self, productions: List[Production]) -> None:
         print("List of productions")
         for item in productions:
-            print(str(item))
+            print(item)
 
     def new_state(self, items: List[Production]) -> StateSintactic:
         state = StateSintactic()
@@ -132,27 +128,6 @@ class Lr0():
 
         # add the states
         self.states = C
-        # # do the aumented and create the first state
-        # I0 = self.aumented_grammar(initial_item)
-        # self.states.append(I0)
-        # state_dummy = StateSintactic()
-        # # * Testing
-        # state_dummy.id = "I7"
-        # state_dummy.items = second
-        # state_dummy.clousure = self.closure(state_dummy.items)
-        # self.states.append(state_dummy)
-
-        # state_dummy2 = StateSintactic()
-        # state_dummy2.id = 'I2'
-        # state_dummy2.items = self.goTo(I0.clousure, TokenSintactic('T'))
-        # state_dummy2.clousure = self.closure(state_dummy2.items)
-        # self.states.append(state_dummy2)
-
-        # state_dummy3 = StateSintactic()
-        # state_dummy3.id = 'I6'
-        # state_dummy3.items = self.goTo(third, TokenSintactic('+'))
-        # state_dummy3.clousure = self.closure(state_dummy3.items)
-        # self.states.append(state_dummy3)
 
 
 
@@ -180,23 +155,26 @@ class Lr0():
         I: List[Production] = []
         for item in listOfItem:
             index_dot = item.index_of(TokenSintactic(dot_ls))
-            if index_dot + 1 < len(item.value):
-                if item.value[index_dot + 1].value == token.value:
-                    temp = Production()
-                    temp.value = item.value.copy()
-                    temp.value[index_dot], temp.value[index_dot + 1] = item.value[index_dot + 1], item.value[index_dot]
-                    I.append(temp)
+            if (
+                index_dot + 1 < len(item.value)
+                and item.value[index_dot + 1].value == token.value
+            ):
+                temp = Production()
+                temp.value = item.value.copy()
+                temp.value[index_dot], temp.value[index_dot + 1] = item.value[index_dot + 1], item.value[index_dot]
+                I.append(temp)
         # return self.closure(I)
         return I
     
     def get_grammar(self, items: List[Production]) -> List[TokenSintactic]:
         grammar: List[TokenSintactic] = []
         for item in items:
-            dot_expression = self.get_dot_expression(item.value)
-            if dot_expression:
-                if dot_expression.next_value:
-                    if dot_expression.next_value not in grammar:
-                        grammar.append(dot_expression.next_value)
+            if dot_expression := self.get_dot_expression(item.value):
+                if (
+                    dot_expression.next_value
+                    and dot_expression.next_value not in grammar
+                ):
+                    grammar.append(dot_expression.next_value)
         return grammar
 
     
@@ -207,13 +185,15 @@ class Lr0():
             if dot_expression := self.get_dot_expression(item.value):
                 for production in self.og_productions:
                     first_token = production.first_token()
-                    if dot_expression.next_value:
-                        if first_token.value == dot_expression.next_value.value:
-                            temp = Production()
-                            temp.value = production.value.copy()
-                            temp.value.insert(2, TokenSintactic(dot_ls))
-                            if temp not in J:
-                                J.append(temp)
+                    if (
+                        dot_expression.next_value
+                        and first_token.value == dot_expression.next_value.value
+                    ):
+                        temp = Production()
+                        temp.value = production.value.copy()
+                        temp.value.insert(2, TokenSintactic(dot_ls))
+                        if temp not in J:
+                            J.append(temp)
         return J
 
     def get_dot_expression(self, items: List[TokenSintactic]) -> DotExpression:

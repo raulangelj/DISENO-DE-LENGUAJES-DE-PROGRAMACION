bcf5f4925283eca6cedc834565b4ca4c179b4980
Raúl Angel Jiménez <raulangelj@gmail.com>
2023-05-13 22:48:02 -0600

    READ files

--- LEXER/yapar.main.py
+++ LEXER/yapar.main.py
@@ -15,27 +15,39 @@ example_lr = [
 ]
 
 def main():
-    # opoen tokens file
-    try:
-        with open(f'{RUTE_DATA}tokens.pkl', 'rb') as file:
-            tokens = pickle.load(file)
-            yapar = Yapar()
-            file_path = input('Ingrese la ruta del archivo: ')
-            yapar.read_file(file_path)
-            # yapar.read_file('LEXER/Mocks/YAPAR/slr-1.yalp')
-            # check if all the tokens in yapar.get_tokens_array() are in tokens
-            for token in yapar.get_tokens_array():
-                if token.lower() not in tokens:
-                    raise Exception(f'Token {token} not found in tokens')
-            productions = yapar.get_productions()
-            lr0 = Lr0(productions)
-            lr0.graph()
-    except Exception as e:
-        print(e)
-    # yapar.read_file('src/YAPAR/grammar.yapar')
-    # yapar.create_automata()
-    # yapar.create_scanner()
-    # yapar.run_scanner('src/YAPAR/program.yapar')
+    running = True
+    while running:
+        print('''
+        1. Leer archivo\n
+        2. Salir\n''')
+        option = input('Ingrese una opción: ')
+        if option == '1':
+            # opoen tokens file
+            try:
+                with open(f'{RUTE_DATA}tokens.pkl', 'rb') as file:
+                    tokens = pickle.load(file)
+                    yapar = Yapar()
+                    file_path = input('Ingrese la ruta del archivo: ')
+                    yapar.read_file(file_path)
+                    # yapar.read_file('LEXER/Mocks/YAPAR/slr-1.yalp')
+                    # check if all the tokens in yapar.get_tokens_array() are in tokens
+                    for token in yapar.get_tokens_array():
+                        if token.lower() not in tokens:
+                            raise Exception(f'Token {token} not found in tokens')
+                    productions = yapar.get_productions()
+                    lr0 = Lr0(productions)
+                    lr0.graph()
+            except Exception as e:
+                print(e)
+            # yapar.read_file('src/YAPAR/grammar.yapar')
+            # yapar.create_automata()
+            # yapar.create_scanner()
+            # yapar.run_scanner('src/YAPAR/program.yapar')
+        elif option == '2':
+            running = False
+            print('Adios')
+        else:
+            print('Ingrese una opción válida')
 
 if __name__ == '__main__':
     main()
